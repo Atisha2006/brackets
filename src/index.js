247d363b5c786062b7125af5a632e25d6846d0ae
Atisha2006 <atisha2006@mail.ru>
2021-03-05 01:52:43 +0300

    doc: correct test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let myArray = [];
+	let myStr = str;
+	for(let item of bracketsConfig) {
+		myArray.push(item.join(""));
+	}
+	for(let i = 0; i <= str.length/2; i++){
+		for(let item of myArray) {
+			myStr = myStr.replace(item, "");
+		}
+	}
+	return myStr.length > 0 ? false : true;
 }
+
